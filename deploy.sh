510e645588d1f4cf9dcfb07f424c6a9d2e31c567
Andrey Prokhorov <andrey@ezy.se>
2017-12-07 00:06:32 +0100

    Updated upload url to netlify

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ zip -r build.zip build
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_KEY" \
      --data-binary "@build.zip" \
-     https://api.netlify.com/api/v1/sites
+     https://api.netlify.com/api/v1/sites/react-hw-7-andpro.netlify.com/deploys
